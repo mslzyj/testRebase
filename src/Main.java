8e09487cc929a6eeed16bc35bf47bd93c79eb8c9
mslzyj <m15829082449@163.com>
2020-11-08 19:15:36 +0800

    init
    aa11
    
    aa22
    
    aa33

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @autor zyj
+ * @date 2020/11/8 19:14
+ */
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("aa11");
+       System.out.println("aa22");
+       System.out.println("aa33");
+    }
+}
